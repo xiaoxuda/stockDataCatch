6195202f10d27c7c52b8cdfa43ec833ba0f15e82
xiaoxuda <xiaoxuda0715@163.com>
2016-04-01 13:33:58 +0800

    Added files via upload

---
+++ src/com/kimi/service/FinancailStatementService.java
@@ -0,0 +1,22 @@
+﻿
+package com.kimi.service;
+
+import com.kimi.dao.FinancailStatementDao;
+import com.kimi.dao.g.BaseDao;
+import com.kimi.entity.FinancailStatement;
+/**
+ * @author kimi
+ * @version 0.99
+ * @see 继承基类
+ */
+public class FinancailStatementService extends BaseService<FinancailStatement,FinancailStatement> {
+	private FinancailStatementDao financailStatementDao;
+
+	protected BaseDao<FinancailStatement,FinancailStatement> getDao(){
+		return this.financailStatementDao;
+	};
+	
+	public void setFinancailStatementDao(FinancailStatementDao dao){
+		this.financailStatementDao=dao;
+	}
+}
