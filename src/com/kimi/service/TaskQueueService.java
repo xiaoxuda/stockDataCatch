bca6c56e07ca159a3e34b117d4e70fe9750940cf
xiaoxuda <xiaoxuda0715@163.com>
2016-04-10 00:04:42 +0800

    Update TaskQueueService.java
    任务队列更换成LinkedBlockingQueue

--- src/com/kimi/service/TaskQueueService.java
+++ src/com/kimi/service/TaskQueueService.java
@@ -2,17 +2,21 @@ package com.kimi.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-
+import java.util.concurrent.LinkedBlockingQueue;
+/**
+ * @see 任务队列，大型爬虫系统中，任务队列需要作为一个独立项目部署在单独服务器中，并用分布式锁保证分布式爬虫系统的正常工作 
+ * @author kimi
+ *
+ */
 public class TaskQueueService {
-	public static Map<String,ArrayBlockingQueue<CatchTask>> queueMap = new HashMap<String,ArrayBlockingQueue<CatchTask>>();
+	public static Map<String,LinkedBlockingQueue<CatchTask>> queueMap = new HashMap<String,LinkedBlockingQueue<CatchTask>>();
 	public static void commitTask(String key,CatchTask task){
 		try {
-			ArrayBlockingQueue<CatchTask> taskqueue = queueMap.get(key);
+			LinkedBlockingQueue<CatchTask> taskqueue = queueMap.get(key);
 			if(taskqueue!=null){
 				taskqueue.put(task);
 			}else{
-				taskqueue = new ArrayBlockingQueue<CatchTask>(100000, true);
+				taskqueue = new LinkedBlockingQueue<CatchTask>(100000);
 				taskqueue.put(task);
 				queueMap.put(key, taskqueue);
 			}
@@ -22,7 +26,7 @@ public class TaskQueueService {
 		}
 	}
 	public static CatchTask getTask(String key){
-		ArrayBlockingQueue<CatchTask> taskqueue = queueMap.get(key);
+		LinkedBlockingQueue<CatchTask> taskqueue = queueMap.get(key);
 		if(taskqueue!=null){
 			return taskqueue.poll();
 		}else{
